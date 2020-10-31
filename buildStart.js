364867aeb82dabba4e5801673f1e7a9e61989540
dikshaupmanyu <diksha.novasoftcorps@gmail.com>
2020-10-31 05:29:19 -0700

    changes

--- buildStart.js
+++ buildStart.js
@@ -7,8 +7,6 @@ const https = require( 'https' );
 const fs = require( 'fs' );
 const app = new koa();
 
-app.use(serve('/build', {
-    maxage: 365 * 24 * 60 * 60
-} ) );
+app.use(serve());
 
 http.createServer( app.callback() ).listen( httpPort, () => console.log( `sever is listening on ${httpPort}` ) );
